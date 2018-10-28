7cf098c5145ae05240f353d6dfd8c4b1b864c8d6
Andrew Hild <republiqueajamais@hotmail.com>
2018-10-28 00:00:50 -0700

    Hotfix: add error checking and make sure only successes get written to log

--- ygo_check.py
+++ ygo_check.py
@@ -27,39 +27,47 @@ with open(filename, 'r') as wishlist:
             print("Use * for any rarity")
             errors.append("Error with card " + tokens[0])
         else:
+
             lowest_found = sys.maxsize
             card_name = tokens[0]
             target_price = float(tokens[1])
             target_rarity = tokens[2].strip() # remove endline character
             # Acquire and format data
-            data = str(YgoApi.get_price_by_name(card_name, base_url))
-            data = data.replace('\'', '\"')
-            pdata = json.loads(data)
-            listings = pdata["data"]
-            # Look for prices @ target rarity
-            rarity = ""
-            price_low = -1
-            for entry in listings:
-                rarity = entry["rarity"]
-                price_low = entry["price_data"]["data"]["prices"]["low"]
-                if price_low <= target_price and (target_rarity == "*" or rarity == target_rarity):
-                    # We only want the lowest price available
-                    if price_low < lowest_found:
-                        lowest_found = price_low
-                        hit = entry
-            notif_string = "Found " + card_name + " in rarity " + hit["rarity"] + " for $" + str(lowest_found)
-            notifs.append(notif_string)
+            try:
+                data = str(YgoApi.get_price_by_name(card_name, base_url))
+                data = data.replace('\'', '\"')
+                pdata = json.loads(data)
+                listings = pdata["data"]
+                # Look for prices @ target rarity
+                rarity = ""
+                price_low = -1
+                for entry in listings:
+                    rarity = entry["rarity"]
+                    price_low = entry["price_data"]["data"]["prices"]["low"]
+                    if price_low <= target_price and (target_rarity == "*" or rarity == target_rarity):
+                        # We only want the lowest price available
+                        if price_low < lowest_found:
+                            lowest_found = price_low
+                            hit = entry
+                            notif_string = "Found " + card_name + " in rarity " + hit["rarity"] + " for $" + str(lowest_found)
+                            notifs.append(notif_string)
+            except Exception:
+                errstr = "Error with card " + card_name
+                errors.append(errstr)
 
-'''
-Handle email sending
-'''
+# Handle email sending
+
 if len(notifs) > 0:
     sender = "noreply@kame-game.net"
-    recipient = "test@example.org"
+    recipient = ""
     subject = "Yu-Gi-Oh Price Alert"
     body = "Subject: " + subject + "\n"
     for item in notifs:
         body = body + item + "\n"
+    if len(errors) > 0:
+        body = body + "Errors:\n"
+        for item in errors:
+            body = body + item + "\n"
     body = body + "\nSincerely,\nKame Game Staff"
 
     msg = EmailMessage()
